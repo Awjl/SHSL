03a11857ec2960dbb18ea2ea9403f9c59d90c86e
佳乐 <le179516844@163.com>
2018-08-16 13:36:29 +0800

    剩余产品中心

--- js/comment.js
+++ js/comment.js
@@ -1,3 +1,4 @@
+// 导航栏
 var show = true
 $('.nav-move').click(function() {
   if (show) {
@@ -7,4 +8,23 @@ $('.nav-move').click(function() {
     $('.headerm').css({'top':'-300%','opacity': '0'})
     show = !show
   }
+})
+// 关于我们
+$('.aboutus-nav ul li').click(function(){
+  $(this).addClass('aboutus-navact').siblings().removeClass('aboutus-navact')
+  $('.aboutus-list>div').removeClass('show').addClass('hide')
+  $('.aboutus-list>div').eq($(this).index()).removeClass('hide').addClass('show')
+})
+$('.technologylist-span span').click(function() {
+  $(this).addClass('span-act').siblings().removeClass('span-act')
+  $('.technologylist>div').removeClass('show').addClass('hide')
+  $('.technologylist>div').eq($(this).index()).removeClass('hide').addClass('show')
+})
+// 提交简历
+$('.uplodebtn').click(function() {
+  $('.ejectbox').css('display','block')
+  // $('body').css('overflow','hidden')
+})
+$('.ejectbox').click(function() {
+  $(this).css('display','none')
 })
