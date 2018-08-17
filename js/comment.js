049742ecd2913889a886d1b7ac9e6630ae70c843
佳乐 <le179516844@163.com>
2018-08-17 17:18:14 +0800

    修改完成

--- js/comment.js
+++ js/comment.js
@@ -1,38 +1,45 @@
 // 导航栏
 var show = true
-$('.nav-move').click(function() {
+$('.nav-move').click(function () {
   if (show) {
-    $('.headerm').css({'top':'65px','opacity': '1'})
+    $('.headerm').css({ 'top': '65px', 'opacity': '1' })
     show = !show
   } else {
-    $('.headerm').css({'top':'-300%','opacity': '0'})
+    $('.headerm').css({ 'top': '-300%', 'opacity': '0' })
     show = !show
   }
 })
 // 关于我们
-$('.aboutus-nav ul li').click(function(){
+$('.aboutus-nav ul li').click(function () {
   $(this).addClass('aboutus-navact').siblings().removeClass('aboutus-navact')
   $('.aboutus-list>div').removeClass('show').addClass('hide')
   $('.aboutus-list>div').eq($(this).index()).removeClass('hide').addClass('show')
 })
-$('.technologylist-span span').click(function() {
+$('.technologylist-span span').click(function () {
   $(this).addClass('span-act').siblings().removeClass('span-act')
   $('.technologylist>div').removeClass('show').addClass('hide')
   $('.technologylist>div').eq($(this).index()).removeClass('hide').addClass('show')
 })
 // 提交简历
-$('.uplodebtn').click(function() {
-  $('.ejectbox').css('display','block')
+$('.uplodebtn').click(function () {
+  $('.ejectbox').css('display', 'block')
   // $('body').css('overflow','hidden')
 })
-$('.ejectbox').click(function() {
-  $(this).css('display','none')
+$('.ejectbox').click(function () {
+  $(this).css('display', 'none')
 })
 // 产品中心
-$('.wrap-productnav div').click(function() {
+$('.wrap-productnav div').click(function () {
   $('.wrap-productnav div').find('span').removeClass('liact')
   console.log($(this))
   $(this).find('span').addClass('liact')
   $('.productclassification-box>div').eq($(this).index()).removeClass('hide').siblings().addClass('hide')
   // productclassification-box
 })
+// 发展历程
+$('.developmentlistbox').on("swipeleft", function () {
+  alert("You swiped left!");
+});
+$(".developmentlistbox").on("swiperight", function () {
+  alert("You swiped right!");
+});
